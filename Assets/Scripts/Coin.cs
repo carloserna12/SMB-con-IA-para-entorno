c465a946f16468074b108c3f2c21f26a55b7d357
carloserna12 <carlos.vivas@correounivalle.edu.co>
2022-05-04 20:53:04 -0500

    Cambios Mayores al manejo de los datos recolectados

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -23,6 +23,7 @@ public class Coin : MonoBehaviour {
 			coor = new Coordenadas("COIN",Math.Round(transform.position.x),Math.Round(transform.position.y));
 			
 			arrayMov.playerLevelData.Add(coor);
+			
 			//arrayMov.playerLevelData.Add("RCoin"+ Math.Round(transform.position.x) + "|" + Math.Round(transform.position.y));
 			/*Debug.Log("ENTRO A LA MONEDA");
 			foreach (var dato in mario.playerLevelData)
